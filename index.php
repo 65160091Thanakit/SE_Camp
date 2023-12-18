03b40d02d69e0330c63d09483c7a3c04d8eaa151
65160091Thanakit <art027.new555@gmail.com>
2023-12-18 19:45:39 +0700

    php_in_class

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+
+<body>
+    <h1>This is PHP<?= 'HELLO ';?></h1>
+    <div>
+        <p>1</p>
+        <?php
+        #this is comment in PHP naja
+
+        #
+
+        //
+
+        /*
+        */
+        $var_A=1;
+        $var_B="1";
+        echo "<h1>Sub Bro</h1>";
+        echo $var_A+"3";
+        $array = array();
+        $array[]=5;
+        $array[0]=1;
+        print_r($array);
+
+
+        ?>
+        <p>2</p>
+        <?php
+
+
+        ?>
+
+        <p>3</p>
+        <?php
+
+
+        ?>
+        
+    </div>
+</body>
+
+
+</html>
